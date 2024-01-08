c6a9002e40641c3ec14b8805ec133bb7569bfe01
Cliff Snyder <cliff.snyder@gmail.com>
2024-01-07 20:56:38 -0500

    labelled statements

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use lc3::Program;
 lalrpop_mod!(pub asm);
 
 fn main() {
-    let parser = asm::InstructionParser::new();
+    let parser = asm::ProgramParser::new();
 
     let mut prog = Program::new();
 
-    let op = parser.parse(&mut prog, "add r0, r1, r2").unwrap();
+    parser.parse(&mut prog, ".orig x3000\nadd r0, r1, r2\nand r3, r4, r5\n.end").unwrap();
 
-    println!("got: {}", op as u16);
+    // println!("got: {}", op as u16);
 }
