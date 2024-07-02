e005c6e4ca96f14d18197f204b9f6c6d93925ffb
Cliff Snyder <cliff.snyder@gmail.com>
2024-07-02 10:40:32 -0400

    using rstest

---
+++ tests/assemble.rs
@@ -0,0 +1,20 @@
+use lc3::{assemble, Program};
+use rstest::rstest;
+use std::{fs, path::PathBuf};
+
+#[rstest]
+fn test_assemble(#[files("examples/*.asm")] infile: PathBuf) {
+    let mut outfile = infile.clone();
+    outfile.set_extension("obj");
+
+    let mut input = fs::File::open(infile).unwrap();
+    let prog = assemble(&mut input).unwrap();
+
+    let mut output = fs::File::open(outfile).unwrap();
+    let mut expected = Program::new();
+    expected.read(&mut output).unwrap();
+
+    assert_eq!(prog.mem, expected.mem);
+
+    // TODO also test symbol table
+}
